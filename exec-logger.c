ac1d5bf36fcc03c4e4c794b478da4daad0560474
Daniel Roberson <daniel@planethacker.net>
2017-07-09 21:15:29 -0700

    store getppid() to not call it so many times

--- exec-logger.c
+++ exec-logger.c
@@ -19,31 +19,34 @@ int execve(const char *progname, char *const argv[], char *const envp[]) {
   uid_t		uid = -1;
   int		commandlen;
   size_t	len;
+  pid_t		ppid;
 
 
+  ppid = getppid();
   openlog("exec-logger", LOG_PID, LOG_AUTHPRIV);
 
   sprintf(loginuid, "/proc/%d/loginuid", getppid());
 
   fp = fopen(loginuid, "r");
   if (fp == NULL) {
-    syslog(LOG_WARNING, "ERROR: Couldn't open /prox/%d/loginuid", getppid());
+    syslog(LOG_WARNING, "ERROR: Couldn't open /prox/%d/loginuid", ppid);
   } else {
     if (fscanf(fp, "%d", &uid) != 1) {
-      syslog(LOG_WARNING, "ERROR: Couldn't read /proc/%d/loginuid", getppid());
+      syslog(LOG_WARNING, "ERROR: Couldn't read /proc/%d/loginuid", ppid);
       uid = -1;
     }
 
     fclose(fp);
   }
 
-  sprintf(buf, "[UID:%d PARENT_PID:%d] CMD:", uid, getppid());
+  sprintf(buf, "[UID:%d PARENT_PID:%d] CMD:", uid, ppid);
   commandlen = strlen(buf);
   
   for (i = 0; argv[i] != NULL; i++) {
     len = strlen(argv[i]);
 
     /* Adding more to the buffer will overflow it, break the loop */
+    /* TODO: inform the log that output has been truncated */
     if ((commandlen + 1 + len) >= LINE_MAX)
       break;
 
@@ -54,6 +57,6 @@ int execve(const char *progname, char *const argv[], char *const envp[]) {
 
   syslog(LOG_INFO, "%s", buf);
 
-  *(void **) (&original_execve) = dlsym(RTLD_NEXT, "execve");  
+  *(void **)(&original_execve) = dlsym(RTLD_NEXT, "execve");  
   return((*original_execve)(progname, argv, envp));
 }
